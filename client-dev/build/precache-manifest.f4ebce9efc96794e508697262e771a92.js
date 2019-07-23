0517c25e2001e45d65ee439961de7aa1254295cf
Thomas Girgenti <thomasg@disroot.org>
2019-07-22 18:37:40 -0600

    requirements for pwa. NOT enabled

---
+++ client-dev/build/precache-manifest.f4ebce9efc96794e508697262e771a92.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd09ee669f8a1fccb45a24b0f7d1189f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "44d2e7f0194fc9e2cc38",
+    "url": "/static/css/2.e05f487f.chunk.css"
+  },
+  {
+    "revision": "1cb98bcce4818102e238",
+    "url": "/static/css/main.73846af3.chunk.css"
+  },
+  {
+    "revision": "44d2e7f0194fc9e2cc38",
+    "url": "/static/js/2.638f18c3.chunk.js"
+  },
+  {
+    "revision": "1cb98bcce4818102e238",
+    "url": "/static/js/main.7e7d1e60.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b00e15a53c5b30cca5f010103c3e8956",
+    "url": "/static/media/astronaut.b00e15a5.svg"
+  },
+  {
+    "revision": "52a26495071f2da9343e3919bb87e805",
+    "url": "/static/media/icon.52a26495.svg"
+  }
+]);
