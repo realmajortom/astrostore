7cb018a70c78d4b874b0288a3e7f399c4da0d349
Thomas Girgenti <thomasg@disroot.org>
2019-07-28 14:36:18 -0600

    update placeholders

---
+++ build/precache-manifest.6b4f5cc59beb56115abd3f9bf16b4a04.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7da9535e2e1ca2eb2a2c70f5908d9a0e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2abe6e381073dde13c99",
+    "url": "/static/css/2.e05f487f.chunk.css"
+  },
+  {
+    "revision": "be1fd2c844e19b8fd01b",
+    "url": "/static/css/main.3d64a9db.chunk.css"
+  },
+  {
+    "revision": "2abe6e381073dde13c99",
+    "url": "/static/js/2.e86c08cd.chunk.js"
+  },
+  {
+    "revision": "be1fd2c844e19b8fd01b",
+    "url": "/static/js/main.04242239.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b00e15a53c5b30cca5f010103c3e8956",
+    "url": "/static/media/astronaut.b00e15a5.svg"
+  },
+  {
+    "revision": "04053f25fb20f5f0de70d53caa122b46",
+    "url": "/static/media/icon.04053f25.svg"
+  }
+]);
