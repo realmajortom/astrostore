4e7f99fa2c483cd4ab0087f6cfec9407afd81cb8
Thomas Girgenti <thomasg@disroot.org>
2019-08-09 21:56:09 -0600

    new client build

---
+++ build/precache-manifest.f59d741dbffa94e93177e12728ab8966.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cae9f21183f2e32d1788c50b216c5781",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fa9330ec5fe745a0cf7a",
+    "url": "/static/css/main.c4da0207.chunk.css"
+  },
+  {
+    "revision": "2c4cee2a42aeed54c513",
+    "url": "/static/js/2.e438f336.chunk.js"
+  },
+  {
+    "revision": "fa9330ec5fe745a0cf7a",
+    "url": "/static/js/main.252892f0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0ded3ffb6ef15e180d8c25279bba65a9",
+    "url": "/static/media/astronaut.0ded3ffb.svg"
+  },
+  {
+    "revision": "9d5d9fe1c1a79d5358c5e9948889d755",
+    "url": "/static/media/moon.9d5d9fe1.png"
+  },
+  {
+    "revision": "b714955102e7151cede3224b775df650",
+    "url": "/static/media/planet.b7149551.png"
+  }
+]);
