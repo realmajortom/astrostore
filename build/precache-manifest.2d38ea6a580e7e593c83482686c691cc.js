9933c9f224ffcef94b3f17154d53847bec49a526
Thomas Girgenti <thomasg@disroot.org>
2019-09-14 23:17:36 -0600

    build containing ios pwa support

---
+++ build/precache-manifest.2d38ea6a580e7e593c83482686c691cc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1df0594b1bd2e68705d61eb9265da0d8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f2c9a532a51b2a56f0b3",
+    "url": "/static/css/main.38c67b2e.chunk.css"
+  },
+  {
+    "revision": "e4bfaf97d48debaf124e",
+    "url": "/static/js/2.19d07ea4.chunk.js"
+  },
+  {
+    "revision": "f2c9a532a51b2a56f0b3",
+    "url": "/static/js/main.132a0d1d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0ded3ffb6ef15e180d8c25279bba65a9",
+    "url": "/static/media/astronaut.0ded3ffb.svg"
+  },
+  {
+    "revision": "9d5d9fe1c1a79d5358c5e9948889d755",
+    "url": "/static/media/moon.9d5d9fe1.png"
+  },
+  {
+    "revision": "0258851616e0c87d86b8a138b95f231a",
+    "url": "/static/media/planet.02588516.png"
+  }
+]);
