ebc1609e35b7dc3537378f0738b97ea6b729934f
Thomas Girgenti <thomasg@disroot.org>
2019-09-23 14:48:27 -0600

    Update rate limits

--- security/rateLimiter.js
+++ security/rateLimiter.js
@@ -1,50 +1,50 @@
 const rateLimit = require('express-rate-limit');
 const mongoStore = require('rate-limit-mongo');
 
-// 5 registration/day/IP
+// 10 registration/day/IP
 const regOp = rateLimit({
   store: new mongoStore({
     uri: process.env.MONGO_URI,
     collectionName: 'reg_rate'
   }),
   windowMs: 24 * 60 * 60 * 1000,
-  max: 5,
+  max: 10,
   headers: false
 });
 
 
-// 10 logins/hr/IP
+// 20 logins/hr/IP
 const loginOp = rateLimit({
   store: new mongoStore({
     uri: process.env.MONGO_URI,
     collectionName: 'login_rate'
   }),
   windowMs: 60 * 60 * 1000,
-  max: 10,
+  max: 20,
   headers: false
 });
 
 
-// 100 collection ops/hr/IP
+// 500 collection ops/hr/IP
 const collectionOp = rateLimit({
   store: new mongoStore({
     uri: process.env.MONGO_URI,
     collectionName: 'collection_rate'
   }),
   windowMS: 60 * 60 * 1000,
-  max: 100,
+  max: 500,
   headers: false
 });
 
 
-// 200 bookmark ops/hr/IP
+// 500 bookmark ops/hr/IP
 const bookmarkOp = rateLimit({
   store: new mongoStore({
     uri: process.env.MONGO_URI,
     collectionName: 'bookmark_rate'
   }),
   windowMs: 60 * 60 * 1000,
-  max: 200,
+  max: 500,
   headers: false
 });
 
