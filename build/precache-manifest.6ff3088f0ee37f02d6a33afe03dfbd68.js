cb583f95b242ac3f7260e544c9d37e6f769fd8f3
Thomas Girgenti <thomasg@disroot.org>
2019-09-23 15:48:16 -0600

    implement add & edit icons

---
+++ build/precache-manifest.6ff3088f0ee37f02d6a33afe03dfbd68.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74d349422c74227b405af7b766d7cbdf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2405393fe3b1d3f7611f",
+    "url": "/static/css/main.05f553eb.chunk.css"
+  },
+  {
+    "revision": "073225549c4ace6cfe83",
+    "url": "/static/js/2.6e2d1af3.chunk.js"
+  },
+  {
+    "revision": "2405393fe3b1d3f7611f",
+    "url": "/static/js/main.a3caf519.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0ded3ffb6ef15e180d8c25279bba65a9",
+    "url": "/static/media/astronaut.0ded3ffb.svg"
+  },
+  {
+    "revision": "9d5d9fe1c1a79d5358c5e9948889d755",
+    "url": "/static/media/moon.9d5d9fe1.png"
+  },
+  {
+    "revision": "0258851616e0c87d86b8a138b95f231a",
+    "url": "/static/media/planet.02588516.png"
+  }
+]);
